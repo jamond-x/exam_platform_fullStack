0fc9c33ae2d66255b50dab090787ac52a6baab18
jamond-x <x2592253600@163.com>
2022-05-05 12:19:35 +0800

    temp

--- src/pages/user/Profile/index.tsx
+++ src/pages/user/Profile/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Avatar } from 'antd';
+import style from './index.less';
+import { Avatar, Card, Image } from 'antd';
+import { useModel } from 'umi';
 
 const Profile: React.FC = () => {
+  const { initialState, setInitialState } = useModel('@@initialState');
+  console.log(initialState.currentUser);
+
   return (
-    <div>
-      <Avatar src="https://joeschmoe.io/api/v1/random" />
+    <div className={style.container}>
+      <Card
+        className={style.card}
+        title="账户信息"
+        extra={<a href="#">More</a>}
+        style={{ width: 700 }}
+      >
+        <div className={style.inside}>
+          <Avatar size={150} src={<Image src={initialState.currentUser.avatar} />} />
+          <div className={style.name}>{initialState.currentUser.name}</div>
+        </div>
+      </Card>
     </div>
   );
 };
